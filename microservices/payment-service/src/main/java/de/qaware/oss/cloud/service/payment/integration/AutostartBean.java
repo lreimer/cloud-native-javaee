c8a90eee1a971820c94f46e949e01225e9f92c02
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2018-03-28 22:06:29 +0200

    Migrated payment service to Payara micro.

---
+++ microservices/payment-service/src/main/java/de/qaware/oss/cloud/service/payment/integration/AutostartBean.java
@@ -0,0 +1,26 @@
+package de.qaware.oss.cloud.service.payment.integration;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.logging.Logger;
+
+@Singleton
+@Startup
+public class AutostartBean {
+
+    @Inject
+    private Logger logger;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @PostConstruct
+    void autostart() {
+        logger.info("Autostarting Payment service entity manager.");
+        entityManager.clear();
+    }
+}
