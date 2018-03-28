22b5cc0ae495d5a9898456ff28e12a5483faf76d
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2018-03-28 20:30:47 +0200

    Added autostart bean.

---
+++ microservices/billing-service/src/main/java/de/qaware/oss/cloud/service/billing/integration/AutostartBean.java
@@ -0,0 +1,25 @@
+package de.qaware.oss.cloud.service.billing.integration;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.logging.Logger;
+
+@Singleton
+@Startup
+public class AutostartBean {
+
+    @Inject
+    private Logger logger;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @PostConstruct
+    void autostart() {
+        logger.info("Autostarting Billing service entity manager.");
+    }
+}
